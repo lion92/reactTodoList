a5b2b0f5093a20c6a026a0418d04b68b058905de
lion92 <38532569+lion92@users.noreply.github.com>
2021-10-12 20:39:01 +0200

    hook api useffect

--- todo/src/components/Form.js
+++ todo/src/components/Form.js
@@ -1,68 +1,68 @@
-import {useState, useEffect} from 'react'
-import Item from './Item';
-
+import { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import Item from "./Item";
 export default function Form(props) {
-   
-let [valueInput, useValue]=useState("bon");
-let [valueInputTitre, useValueTitre]=useState("bon");
-const context = {titre: valueInputTitre, text: valueInput};
-let [text, useText]=useState([
+  let [valueInput, useValue] = useState("bon");
+  let [valueInputTitre, useValueTitre] = useState("bon");
+  let [textp, setText] = useState([
+    { titre: "bonjour1", text: "hehe" },
+    { titre: "bonjour2", text: "hehe" },
+    { titre: "bonjour3", text: "hehe" },
+  ]);
+  const fetchAPI = useCallback(async () => {
+    const response = await fetch("https://memo.krissclotilde.com/tout/tache");
+    const resbis = await response.json();
+    setText(resbis.message);
+  }, [setText]);
 
-]);
-   let Valuechange=(e)=>{
-       
-       let a=e.target.value;
-       console.log(a);
+useEffect(fetchAPI, []);
+  const context = { titre: valueInputTitre, text: valueInput };
+
+  let Valuechange = (e) => {
+    let a = e.target.value;
+    console.log(a);
     useValue(a);
 
-         
+    return a;
+  };
+  let Valuechangetitre = (e) => {
+    let a = e.target.value;
+    console.log(a);
 
-        return a;
-    }
-   let Valuechangetitre=(e)=>{
-       
-       let a=e.target.value;
-       console.log(a);
+    useValueTitre(a);
 
-       
-       useValueTitre(a)
+    return a;
+  };
 
-        return a;
-    }
+  let HandleSubmit = (e) => {
+    e.preventDefault();
+    let nouveau = [...textp];
+    let newinput = {};
+    newinput.text = valueInput;
+    newinput.titre = valueInputTitre;
 
-    let HandleSubmit=(e)=>{
-        e.preventDefault();
-       let nouveau=[...text];
-       let newinput= {};
-       newinput.text=valueInput;
-       newinput.titre=valueInputTitre;
+    nouveau.push(newinput);
 
-       nouveau.push(newinput);
-
-       useText(nouveau);
-
-    }
-    return (
-        <>
-        <div className="container">
-         <form className="container" onSubmit={HandleSubmit}>
+    setText(nouveau);
+  };
+  return (
+    <>
+      <form onSubmit={HandleSubmit}>
         <label>
-          Essay: 
-          <input value={valueInputTitre} onChange={(e)=>{Valuechangetitre(e);}} />  
-          <input value={valueInput} onChange={(e)=>Valuechange(e)} />        </label>
-         
+          Essay:
+          <input value={valueInput} onChange={(e) => Valuechange(e)} />{" "}
+        </label>
+        <input
+          value={valueInputTitre}
+          onChange={(e) => {
+            Valuechangetitre(e);
+          }}
+        />
         <input type="submit" value="Envoyer" />
-        {
-      
-            text.map((item, index)=>{
-                return(
-                    <Item titre={item.titre} text={item.text}></Item>
-                )
-            })
-        }
+        {textp.map((item, index) => {
+          return <Item titre={item.titre} text={item.text}></Item>;
+        })}
       </form>
-      </div>
-        </>
-    )
+    </>
+  );
 }
-
