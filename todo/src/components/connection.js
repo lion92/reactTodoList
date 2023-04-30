169e28f05eb3baeec2992786e53ffcbe52114c15
kriss <kriss.clotilde@gmail.com>
2023-04-30 22:01:24 +0200

    debut de la page connection suite email password login signup et cadre

--- todo/src/components/connection.js
+++ todo/src/components/connection.js
@@ -4,11 +4,17 @@ const Connection = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     return (
-        <div id='container'>
-            <div id="iconLogin"></div>
-            <input id='email' value={email} placeholder={'email'} onChange={e => setEmail(e.target.value)} type={'text'} />
-            <input id='password' value={password} placeholder={'password'} onChange={e => setPassword(e.target.value)} type={'text'} />
-            <a id="mdpOublie" href="">Mot de passe ounblié</a>
+        <div>
+            <div id='container'>
+                <div id="iconLogin"/>
+                <input id='email' value={email} placeholder={'email'} onChange={e => setEmail(e.target.value)}
+                       type={'text'}/>
+                <input id='password' value={password} placeholder={'password'}
+                       onChange={e => setPassword(e.target.value)} type={'text'}/>
+                <a id="mdpOublie" href="">Mot de passe ounblié</a>
+                <button id='btnLogin'>LOGIN</button>
+                <button id='btnSignup'>SIGNUP</button>
+            </div>
         </div>
     );
 };
