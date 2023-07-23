d01d630663d08082512ac3037a1834dc26cad3a9
kriss <kriss.clotilde@gmail.com>
2023-07-23 20:31:45 +0200

    ajout du message d erreur lors de l'echec de la connection

--- todo/src/components/connection.js
+++ todo/src/components/connection.js
@@ -1,71 +1,70 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import Navigation from "./Navigation";
 import Form from "./Form";
-const Connection=(req,res)=>
-{
+
+const Connection = () => {
+    const [messageLog, setMessageLog]=useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [probleme, setProbleme] = useState("");
+    let fetchConnection = useCallback(async (e) => {
+        e.preventDefault();
+        const response = await fetch(
+            "http://localhost:3004/connection/login",
+            {
+                method: "POST",
+                body: JSON.stringify({
 
-    const login = async () => {
+                    "password": password,
+                    "email": email
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+        await response.json().then(data => {
 
-        const method = 'Post'
-        const bodyAenvoyer = {
-            "password": password,
-            "email": email
-        }
-        const options = {
-            method: method,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Access-Control-Allow-Origin": "*"
-            },
-            body: JSON.stringify(bodyAenvoyer
-            )
-        }
-        try {
-            const url = "http://localhost:3004/connection/login"
-
-            const response = await fetch(url, options);
-            const data = await response.json();
-            await console.log("front");
-            await console.log(data)
             if (!isNaN(data?.id)) {
-                await setProbleme('connecte')
+                setMessageLog("Code Bon");
+                setProbleme('connecte')
+            }
+            else{
+                setMessageLog("Combinaison code et mot de passe incorrect")
+
             }
 
+        })
+    });
 
-        } catch (err) {
-            console.log(err)
-        }
-
-    }
     return (
         <div>
-            {(probleme === "connecte") ? (<Form></Form>) : ''
-            }
-            {(probleme !== "connecte") ? (
-                <>
+                {(probleme === "connecte") ? (<Form></Form>) : ''
+                }
+                {(probleme !== "connecte") ? (
+                    <>
                     <div id='container'>
                         <Navigation></Navigation>
+                        <div>{messageLog}</div>
                         <div id="iconLogin"/>
                         <input id='email' value={email} placeholder={'email'} onChange={e => setEmail(e.target.value)}
                                type={'text'}/>
                         <input id='password' value={password} placeholder={'password'}
                                onChange={e => setPassword(e.target.value)} type={'text'}/>
                         <a id="mdpOublie" href="">Mot de passe ounblié</a>
-                        <button onClick={login} id='btnLogin'>LOGIN</button>
-                        <h1>{(probleme !== 'connecte' ? 'incorrect' : 'connecte')}</h1>
+                        <button onClick={fetchConnection} id='btnLogin'>LOGIN</button>
+                        <h1>{(probleme!=='connecte'?'incorrect':'connecte')}</h1>
                         <a href='/inscription'>
                             SIGNUP
                         </a>
                     </div>
-                </>
 
-            ) : ''
-            }
+                    </>
+
+                ) : ''
+                }
         </div>
-    )
-}
+    );
+};
+
 export default Connection;
--- todo/src/components/inscription.js
+++ todo/src/components/inscription.js
@@ -1,71 +1,57 @@
 import React, {useCallback, useState} from 'react';
 import Navigation from "./Navigation";
 
-const Inscription = (req,res) => {
+const Inscription = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [nom, setNom] = useState("");
     const [prenom, setPrenom] = useState("");
     const [age, setAge] = useState("");
-    const signupFetch=async()=>{
-            const method = 'Post'
-            const bodyAenvoyer = {
-                "nom": nom,
-                "prenom": prenom,
-                "age": age,
-                "password": password,
-                "email": email
-            }
-            const options = {
-                method: method,
+
+    let fetchInscription= useCallback(async (e) => {
+        e.preventDefault();
+        const response = await fetch(
+            "http://localhost:3004/connection/signup",
+            {
+                method: "POST",
+                body: JSON.stringify(
+                    {
+                        "nom": nom,
+                        "prenom": prenom,
+                        "age": age,
+                        "password": password,
+                        "email": email
+                    },),
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    "Access-Control-Allow-Origin": "*"
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify(bodyAenvoyer
-                )
             }
+            );
+    });
 
-            try {
-                const url = 'http://localhost:3004/connection/signup'
+    return (
 
-                const response = await fetch(url, options);
-                const data = await response.json();
-                await console.log(data);
+        <div>
+        <div id='container'>
+        <Navigation></Navigation>
+        <div id="iconLogin"/>
+    <input id='nom' value={nom} placeholder={'nom'}
+    onChange={e => setNom(e.target.value)} type={'text'}/>
+    <input id='prenom' value={prenom} placeholder={'prenom'}
+    onChange={e => setPrenom(e.target.value)} type={'text'}/>
+    <input id='age' value={age} placeholder={'age'}
+    onChange={e => setAge(e.target.value)} type={'text'}/>
 
-            } catch (err) {
-              console.log(err)
-            }
+    <input id='email' value={email} placeholder={'email'} onChange={e => setEmail(e.target.value)}
+    type={'text'}/>
+    <input id='password' value={password} placeholder={'password'}
+    onChange={e => setPassword(e.target.value)} type={'text'}/>
 
-        }
+    <button onClick={fetchInscription} id='btnSignup'>SIGNUP</button>
+        <a href='/login' >LOGIN</a>
+        </div>
+        </div>
+);
+};
 
-
-
-
-        return (
-
-            <div>
-                <div id='container'>
-                    <Navigation></Navigation>
-                    <div id="iconLogin"/>
-                    <input id='nom' value={nom} placeholder={'nom'}
-                           onChange={e => setNom(e.target.value)} type={'text'}/>
-                    <input id='prenom' value={prenom} placeholder={'prenom'}
-                           onChange={e => setPrenom(e.target.value)} type={'text'}/>
-                    <input id='age' value={age} placeholder={'age'}
-                           onChange={e => setAge(e.target.value)} type={'text'}/>
-
-                    <input id='email' value={email} placeholder={'email'} onChange={e => setEmail(e.target.value)}
-                           type={'text'}/>
-                    <input id='password' value={password} placeholder={'password'}
-                           onChange={e => setPassword(e.target.value)} type={'text'}/>
-
-                    <button onClick={signupFetch} id='btnSignup'>SIGNUP</button>
-                    <a href='/login'>LOGIN</a>
-                </div>
-            </div>
-        );
-    };
-
-    export default Inscription;
+export default Inscription;
